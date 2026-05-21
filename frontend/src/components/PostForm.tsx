10034f7dbfefcab753b5f8cb6c90f9a3cd3c9d47
kouzai <username@users.noreply.github.com>
2026-05-22 01:13:17 +0900

    登録フォームレイアウト修正

--- frontend/src/components/PostForm.tsx
+++ frontend/src/components/PostForm.tsx
@@ -7,17 +7,34 @@ type Props = {
 export default function PostForm(props: Props) {
   return (
     <div className="form" onClick={(e) => e.stopPropagation()}>
-      <h2>思い出 登録</h2>
-      <input type="text" placeholder="タイトル" />
-      <textarea rows={5} placeholder="内容" />
-      <select>
-        <option>広島県</option>
-      </select>
-      <select>
-        <option>ライブ</option>
-      </select>
-      <input type="date" />
-      <button>登録</button>
+      <h2>思い出登録</h2>
+      <div className="form-row">
+        <label>タイトル</label>
+        <input type="text" placeholder="タイトル" />
+      </div>
+      <div className="form-row">
+        <label>日付</label>
+        <input type="date" />
+      </div>
+      <div className="form-row">
+        <label>内容</label>
+        <textarea rows={5} cols={50} placeholder="内容" />
+      </div>
+      <div className="form-row">
+        <label>都道府県</label>
+        <select>
+          <option>広島県</option>
+        </select>
+      </div>
+      <div className="form-row">
+        <label>タグ</label>
+        <select>
+          <option>ライブ</option>
+        </select>
+      </div>
+      <div className="button-area">
+        <button className="submitbtn">登録</button>
+      </div>
       <button className="closebtn" onClick={() => props.closeForm()}>
         ✕
       </button>
